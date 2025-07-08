12f46785f58c6e565421e435750eed917548a032
atdilshan <atdilshan0127@gmail.com>
2025-07-09 00:19:20 +0530

    fix:fixed

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 require __DIR__ . '/public/index.php';
--- public/index.php
+++ public/index.php
@@ -1,9 +1,4 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
 require_once __DIR__ . '/../renderLayoutsHierarchy.php';
 require_once __DIR__ . '/../utils/helpers/meta.php';
 
